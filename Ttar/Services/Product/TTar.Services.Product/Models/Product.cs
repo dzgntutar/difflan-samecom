99b83846dc004e590cc9fee7532337b53b6ec32e
dzgntutar <duzguntutar@gmail.com>
2022-04-06 23:15:13 +0300

    mongo db objects

---
+++ Ttar/Services/Product/TTar.Services.Product/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace TTar.Services.Product.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        [BsonRepresentation(MongoDB.Bson.BsonType.Decimal128)]
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.DateTime)]
+
+        public DateTime CreateDate { get; set; }
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+
+        public string CategoryId { get; set; }
+
+        [BsonIgnore]
+        public Category Category { get; set; }
+    }
+}
