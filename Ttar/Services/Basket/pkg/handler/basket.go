36ef41e5de11a2c90dbc1e079ec9a393832495bf
dzgntutar <duzguntutar@gmail.com>
2022-05-01 22:20:32 +0300

    gorilla

--- Ttar/Services/Basket/pkg/handler/basket.go
+++ Ttar/Services/Basket/pkg/handler/basket.go
@@ -4,7 +4,6 @@ import (
 	. "basket/pkg/model"
 	"basket/pkg/redis"
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
@@ -15,25 +14,28 @@ func CreateHandler(redisConfig *redis_p.RedisConfig) http.HandlerFunc {
 		var basket Basket
 
 		decoder := json.NewDecoder(r.Body)
-
-		if err := decoder.Decode(&basket); err != nil {
-			return
-		}
 		defer r.Body.Close()
 
-		var totalP float32
-		for _, value := range basket.BasketItem {
-			totalP += float32(value.Quantity) * value.Price
+		if err := decoder.Decode(&basket); err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Bad request"))
+		} else {
+			var totalP float32
+			for _, value := range basket.BasketItem {
+				totalP += float32(value.Quantity) * value.Price
+			}
+			basket.TotalPrice = totalP
 		}
-		basket.TotalPrice = totalP
 
-		fmt.Println(basket)
-
-		data, _ := json.Marshal(basket)
-
-		strData := string(data)
-
-		redisConfig.SetStringValue(basket.UserId, strData)
+		if data, err := json.Marshal(basket); err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Bad request"))
+		} else {
+			strData := string(data)
+			redisConfig.SetStringValue(basket.UserId, strData)
+		}
 	}
 }
 
@@ -41,12 +43,20 @@ func GetHandler(redisConfig *redis_p.RedisConfig) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		id, err := strconv.Atoi(vars["id"])
+
 		if err != nil {
-			fmt.Println(err)
-			return
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Bad request"))
 		}
-		fmt.Println(id)
-		data, _ := redisConfig.GetStringValue("basket")
-		fmt.Println(data)
+		if data, err := redisConfig.GetStringValue(string(id)); err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Basket not found"))
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(data))
+		}
 	}
 }
